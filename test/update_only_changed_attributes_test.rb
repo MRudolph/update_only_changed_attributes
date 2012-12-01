5d17ee4e923032399d152322d48a55a0f6106f1d
Maximilian Rudolph <MaximilianRudolph@gmx.net>
2012-12-01 13:52:28 +0000

    fixed tests

--- test/update_only_changed_attributes_test.rb
+++ test/update_only_changed_attributes_test.rb
@@ -42,14 +42,14 @@ class UpdateOnlyChangedAttributesTest < ActiveSupport::TestCase
   test "new:updating unprotected attributes should still work" do
     thing = Thing.create({a:"a",b:"b",c:"c"},without_protection: true)
     assert_nothing_raised(ActiveModel::MassAssignmentSecurity::Error) do
-      thing.update_attributes({a:"newA"})
+      thing.update_only_changed_attributes({a:"newA"})
     end
     assert_equal thing.a,"newA"
   end
   test "new:mixing unprotected and protected attributes should work if the protected are not modified" do
     thing = Thing.create({a:"a",b:"b",c:"c"},without_protection: true)
     assert_nothing_raised(ActiveModel::MassAssignmentSecurity::Error) do
-      thing.update_attributes({a:"newA",b:"b"})
+      thing.update_only_changed_attributes({a:"newA",b:"b"})
     end
     assert_equal thing.a,"newA"
     assert_equal thing.b,"b"
@@ -63,5 +63,11 @@ class UpdateOnlyChangedAttributesTest < ActiveSupport::TestCase
     assert_equal thing.a,"a"
     assert_equal thing.b,"b"
     assert_equal thing.c,"c"
-  end     
+  end
+  test "new: attribute_change_delta returns only the changed data" do
+    thing = Thing.create({a:"a",b:"b",c:"c"},without_protection: true)
+    change =thing.attribute_change_delta({a: "newA",b:"b"})
+    assert_equal change, {a:"newA"}
+  end  
+  
 end
