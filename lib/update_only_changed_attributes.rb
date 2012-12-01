5f9f2260a634d962d8987e474cd8e96983dc82e2
Maximilian Rudolph <MaximilianRudolph@gmx.net>
2012-12-01 13:52:51 +0000

    added code

--- lib/update_only_changed_attributes.rb
+++ lib/update_only_changed_attributes.rb
@@ -1,7 +1,16 @@
 module UpdateOnlyChangedAttributes
 
+  def attribute_change_delta(params)
+    result = {}
+    params.each do |key,value|
+      result[key] = value if self[key] != value
+    end
+    result
+  end
+  
   def update_only_changed_attributes(params)
-    
+    changes = attribute_change_delta(params)
+    self.update_attributes(changes)
   end 
 end
 ActiveRecord::Base.send :include, UpdateOnlyChangedAttributes
