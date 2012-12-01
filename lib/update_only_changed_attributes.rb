282f9d65196fb32950626f6d9d9d1e0d1b9e9a92
Maximilian Rudolph <MaximilianRudolph@gmx.net>
2012-12-01 14:04:02 +0000

    added basic desciption to methods

--- lib/update_only_changed_attributes.rb
+++ lib/update_only_changed_attributes.rb
@@ -1,5 +1,7 @@
 module UpdateOnlyChangedAttributes
 
+  # attribute_change_delta compares the given hash against the attributes found in the model.
+  # It just returns the values that are different.
   def attribute_change_delta(params)
     result = {}
     params.each do |key,value|
@@ -8,9 +10,18 @@ module UpdateOnlyChangedAttributes
     result
   end
   
+  # update_only_changed_attributes takes a hash like update_attributes, but just passes attributes which have changed.  
+  # Doesn't currently support nested attributes.
   def update_only_changed_attributes(params)
     changes = attribute_change_delta(params)
     self.update_attributes(changes)
+  end
+  # update_only_changed_attributes! takes a hash like update_attributes!, but just passes attributes which have changed.
+  # Doesn't currently support nested attributes.
+  def update_only_changed_attributes!(params)
+    changes = attribute_change_delta(params)
+    self.update_attributes(changes)
   end 
+  
 end
 ActiveRecord::Base.send :include, UpdateOnlyChangedAttributes
